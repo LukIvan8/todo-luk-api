f421472844f850845cadeca18af735b97bf5e743
lukivan8 <lukivan888@gmail.com>
2023-01-13 13:30:28 +0600

    Убрал лишний эндпоинт

--- src/core/todo/todo.controller.ts
+++ src/core/todo/todo.controller.ts
@@ -1,15 +1,10 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { TodoService } from "./todo.service";
 import { CreateTodoDto } from "./dto/create-todo.dto";
 
 @Controller("todo")
 export class TodoController {
   constructor(private todoService: TodoService) {
-  }
-
-  @Get("hi")
-  todoHi() {
-    return "Todo Connected";
   }
 
   @Post("create")
